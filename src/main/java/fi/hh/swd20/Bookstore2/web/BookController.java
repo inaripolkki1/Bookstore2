fcec63f02c2e9055e564046fd5c562708c6443c5
inaripolkki1 <bgm038@myy.haaga-helia.fi>
2021-03-16 23:26:20 +0200

    kustomisoitu login

--- src/main/java/fi/hh/swd20/Bookstore2/web/BookController.java
+++ src/main/java/fi/hh/swd20/Bookstore2/web/BookController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -22,6 +23,11 @@ public class BookController {
 	private BookRepository brepository;
 	@Autowired
 	private CategoryRepository crepository;
+	
+	@GetMapping("/login")
+	public String login() {
+		return "login";
+	}
 	
 	@RequestMapping("/booklist")
 	public String bookList(Model model) {
